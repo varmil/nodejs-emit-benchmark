cb8f4bb57ead29315cd4dec886cc437711cd9c32
A.Yamamoto <fkmks247@gmail.com>
2015-07-30 13:08:44 +0900

    For nodejs-emit-benchmark

--- app.js
+++ app.js
@@ -1,68 +1,132 @@
 var profiler = require('v8-profiler');
+var microtime = require('microtime');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var _ = require('lodash');
 
-server.listen(3000);
+/**
+ * Handle argument
+ */
+var argvIndex = 2;
+var CLIENT_COUNT_PER_ROOM;
+if (! process.argv[argvIndex]) {
+  return console.log('Usage: node app.js [client count per room (int)]');
+} else {
+  CLIENT_COUNT_PER_ROOM = process.argv[argvIndex];
+}
 
+// socket.ioのルーム用変数
+var ROOM_NAME_PREFIX = 'room_';
+var roomIndex = 0;
+
+// ベンチマーク用変数
 var users = 0;
 var countReceived = 0;
 var countSended = 0;
 
-function roundNumber(num, precision) {
-  return parseFloat(Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision));
-}
+// 特定の部屋に接続しているソケット数を取得
+var getRoommateCount = function(room) {
+  return _.keys(io.sockets.adapter.rooms[room]).length;
+};
 
-setInterval(function() {
-  var auxReceived = roundNumber(countReceived / users, 1);
-  var msuReceived = (users > 0 ? auxReceived : 0);
+// 全ルーム一覧リストを返す
+var getAllRooms = function(io) {
+  return _.chain(io.sockets.adapter.rooms)
+    .keys()
+    .filter(function(roomName) {
+      return roomName.indexOf(ROOM_NAME_PREFIX) !== -1;
+    })
+    .value();
+};
 
-  var auxSended = roundNumber(countSended / users, 1);
-  var msuSended = (users > 0 ? auxSended : 0);
+server.listen(3000);
 
-  var l = [
-    'U: ' + users,
-    'MR/S: ' + countReceived,
-    'MS/S: ' + countSended,
-    'MR/S/U: ' + msuReceived,
-    'MS/S/U: ' + msuSended,
-  ];
+/**
+ * Display benchmark infomation
+ */
+(function() {
+  function roundNumber(num, precision) {
+    return parseFloat(Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision));
+  }
 
-  console.log(l.join(',\t'));
-  countReceived = 0;
-  countSended = 0;
+  setInterval(function() {
+    var auxReceived = roundNumber(countReceived / users, 1);
+    var msuReceived = (users > 0 ? auxReceived : 0);
 
-}, 1000);
+    var auxSended = roundNumber(countSended / users, 1);
+    var msuSended = (users > 0 ? auxSended : 0);
 
+    var l = [
+      'U: ' + users,
+      'MR/S: ' + countReceived,
+      'MS/S: ' + countSended,
+      'MR/S/U: ' + msuReceived,
+      'MS/S/U: ' + msuSended,
+    ];
 
-app.all('*', function(req, res, next) {
-  // res.setHeader('Connection', 'close');
-  // res.end('accepted');
+    console.log(l.join(',\t'));
+    countReceived = 0;
+    countSended = 0;
 
-  // req.startTime = microtime.now();
+  }, 1000);
+})();
 
-  next();
-});
-
-
-app.get('/', function (req, res) {
-  countReceived++;
-
-  // EMIT TO ALL SOCKETS
-  io.sockets.emit('everyone');
-
-  countSended += users;
-
-  res.setHeader('Connection', 'close');
-  res.end('accepted');
-});
-
-
-io.sockets.on('connection', function(socket) {
-
-  users = socket.client.conn.server.clientsCount;
-
-  socket.on('disconnect', function() {
-    users = socket.client.conn.server.clientsCount;
+/**
+ * Express Routing
+ */
+(function() {
+  app.all('*', function(req, res, next) {
+    req.startTime = microtime.now();
+    next();
   });
-});
+
+
+  app.get('/', function (req, res, next) {
+    countReceived++;
+
+    // emit to a random existing room
+    var roomName =  ROOM_NAME_PREFIX + _.random(0, getAllRooms(io).length - 1);
+    io.to(roomName).emit('hi roommate');
+
+    countSended += getRoommateCount(roomName);
+
+    next();
+  });
+
+  app.all('*', function(req, res, next) {
+    res.setHeader('Connection', 'close');
+    res.end('accepted');
+
+    var responseTime = microtime.now() - req.startTime;
+    if (responseTime > 1000) console.log('[RESPONSE_TIME over 1ms]', responseTime, 'μs');
+
+    next();
+  });
+})();
+
+/**
+ * socket.io
+ */
+(function() {
+  io.sockets.on('connection', function(socket) {
+
+    users = socket.client.conn.server.clientsCount;
+
+    socket.on('disconnect', function() {
+      users = socket.client.conn.server.clientsCount;
+    });
+
+    // join room
+    var roomName = ROOM_NAME_PREFIX + roomIndex;
+    socket.join(roomName);
+
+    var roommateCount = getRoommateCount(roomName);
+    console.log('[ROOM]', roomName, ' [COUNT]', roommateCount);
+
+    // Increment roomIndex when room is full
+    if (roommateCount >= CLIENT_COUNT_PER_ROOM) {
+      roomIndex++;
+    }
+  });
+})();
--- benchmark.js
+++ benchmark.js
@@ -2,7 +2,7 @@ var profile = require('v8-profiler');
 var io = require('socket.io-client');
 var message = 'o bispo de constantinopla nao quer se desconstantinopolizar';
 
-function user(emitInterval, shouldBroadcast, host, port) {
+function user(host, port) {
   // Avoid Upgrading to WebSocket. (always connect with WebSocket)
   var socket = io.connect('http://' + host + ':' + port, {
     'force new connection': true,
@@ -16,28 +16,7 @@ function user(emitInterval, shouldBroadcast, host, port) {
 
 
   socket.on('connect', function() {
-    // Start messaging loop
-    // if (shouldBroadcast) {
-      // message will be broadcasted by server
-      // socket.emit('broadcast', message);
-    // } else {
-      // message will be echoed by server
-      // socket.send(message);
-    // }
 
-    // socket.on('message', function(message) {
-      // var interval = Math.round(Math.random() * emitInterval * 0.1) + emitInterval;
-      // setTimeout(function() {
-      //   socket.send(message);
-      // }, interval);
-    // });
-
-    // socket.on('broadcastOk', function() {
-      // var interval = Math.round(Math.random() * emitInterval * 0.1) + emitInterval;
-      // setTimeout(function() {
-      //   socket.emit('broadcast', message);
-      // }, interval);
-    // });
   });
 
   socket.on('connect_error', function(reason) {
@@ -55,11 +34,9 @@ var argvIndex = 2;
 var users = parseInt(process.argv[argvIndex++]);
 var rampUpTime = parseInt(process.argv[argvIndex++]) * 1000; // in seconds
 var newUserTimeout = rampUpTime / users;
-var emitInterval = process.argv[argvIndex++] ? process.argv[argvIndex - 1] * 1  : 100;
-var shouldBroadcast = process.argv[argvIndex++] === 'broadcast' ? true : false;
 var host = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : 'localhost';
 var port = process.argv[argvIndex++] ? process.argv[argvIndex - 1]  : '3000';
 
 for(var i=0; i<users; i++) {
-  setTimeout(function() { user(emitInterval, shouldBroadcast, host, port); }, i * newUserTimeout);
+  setTimeout(function() { user(host, port); }, i * newUserTimeout);
 }
